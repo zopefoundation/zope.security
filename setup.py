da19b72be6bf2252e55dbfbfc06b0d4ad873e7a2
Christian Theune <ct@gocept.com>
2007-04-22 11:08:20 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.security',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.security',
       license='ZPL 2.1',
       description='Zope3 Security Architecture',
