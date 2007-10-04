775556daaeafe53d192733f99b71529aa8ebac8a
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-10-04 06:20:04 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.security',
-      version = '3.4.0',
+      version = '3.4.1dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope3 Security Architecture',
