73eb572637d3f436af8b5ffb8d60881930a7c415
Martijn Faassen <faassen@startifact.com>
2009-01-31 14:14:21 +0000

    Post release vb.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.security',
-      version = '3.6.0dev',
+      version = '3.6.1dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope3 Security Architecture',
