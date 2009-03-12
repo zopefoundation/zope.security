8effe6f737835301f585019312e0452090780678
Dan Korostelev <nadako@gmail.com>
2009-03-12 03:23:29 +0000

    Add `addCheckerPublic` helper function in ``zope.security.testing`` module that registers the "zope.Public" permission as an IPermission utility. This came from zope.app.security.

--- src/zope/security/testing.py
+++ src/zope/security/testing.py
@@ -18,8 +18,9 @@ This module provides some helper/stub objects for setting up interactions.
 $Id$
 """
 
-from zope import interface
+from zope import interface, component
 from zope.security import interfaces
+from zope.security.permission import Permission
 
 class Principal:
 
@@ -40,3 +41,17 @@ class Participation:
     def __init__(self, principal):
         self.principal = principal
         self.interaction = None
+
+
+def addCheckerPublic():
+    """Add the CheckerPublic permission as 'zope.Public'"""
+
+    perm = Permission('zope.Public', 'Public',
+            """Special permission used for resources that are always public
+
+            The public permission is effectively an optimization, sine
+            it allows security computation to be bypassed.
+            """
+            )
+    gsm = component.getGlobalSiteManager()
+    gsm.registerUtility(perm, interfaces.IPermission, perm.id)
