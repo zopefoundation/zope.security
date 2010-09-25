7ebba3b77de3e3d4811b95da6e6e8a1625fcb24e
Michael Howitz <mh@gocept.com>
2010-09-25 11:18:47 +0000

    bring branch in shape

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.security',
-      version = '3.7.3',
+      version = '3.7.4dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Security Framework',
