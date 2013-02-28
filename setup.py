ca75fe1d311c485471b53939f5127381cbaa0c40
Stephan Richter <stephan.richter@gmail.com>
2013-02-28 13:48:00 -0500

    Back to development: 4.0.0a6

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ else:
     ]
 
 setup(name='zope.security',
-      version='4.0.0a5',
+      version='4.0.0a6.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Security Framework',
