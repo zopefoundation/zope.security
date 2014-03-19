007f4ffc3fb7dee3deeb7fbd88926fb07a0848a5
Tres Seaver <tseaver@palladion.com>
2014-03-19 15:08:07 -0400

    svb

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ else:
     ]
 
 setup(name='zope.security',
-      version='4.0.1',
+      version='4.0.2dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Security Framework',
