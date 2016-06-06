7a677d8e92774290c6ce4e92b5ebac8b25209277
Marius Gedminas <marius@gedmin.as>
2016-06-06 10:47:36 +0300

    Fix test failures on 64-bit Windows
    Hopefully.  Appveyor should tell me if I've succeeded.
    
    Fixes #17.

--- src/zope/security/proxy.py
+++ src/zope/security/proxy.py
@@ -51,10 +51,12 @@ def _fmt_address(obj):
     # Try to replicate PyString_FromString("%p", obj), which actually uses
     # the platform sprintf(buf, "%p", obj), which we cannot access from Python
     # directly (and ctypes seems like overkill).
-    if sys.platform == 'win32':
+    if sys.platform != 'win32':
+        return '0x%0x' % id(obj)
+    elif sys.maxsize < 2**32:
         return '0x%08X' % id(obj)
     else:
-        return '0x%0x' % id(obj)
+        return '0x%016X' % id(obj)
 
 
 class ProxyPy(PyProxyBase):
