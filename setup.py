46057595827ba760d8d6b1820aca0eefa96c9dc6
Jason Madden <jamadden@gmail.com>
2018-08-24 11:03:58 -0500

    Back to development: 4.3.1

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ TESTS_REQUIRE = [
 
 
 setup(name='zope.security',
-      version='4.3.0',
+      version='4.3.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Security Framework',
