0074a43cd4fa3a98bc5eacae04d636175f32fe66
Jens Vagelpohl <jens@plyp.com>
2022-11-08 11:57:30 +0100

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ TESTS_REQUIRE = [
 
 
 setup(name='zope.security',
-      version='5.5',
+      version='5.6.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Security Framework',
