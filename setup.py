831a34f5cf3e2c4297e0da35c4f38e7cf12fac16
Michael Howitz <mh@gocept.com>
2022-11-30 08:24:31 +0100

    Back to development: 5.9

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ TESTS_REQUIRE = [
 
 
 setup(name='zope.security',
-      version='5.8',
+      version='5.9.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Security Framework',
