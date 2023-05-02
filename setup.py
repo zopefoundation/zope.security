f183f6724a8478d7758e8a249ffeb0319b07a32c
Michael Howitz <mh@gocept.com>
2023-05-02 15:09:28 +0200

    Back to development: 4.0.5

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ else:
     ]
 
 setup(name='zope.security',
-      version='4.0.4',
+      version='4.0.5.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Security Framework',
